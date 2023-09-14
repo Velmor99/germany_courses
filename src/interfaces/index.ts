caf7e970a9e017101e52081bce7ad3458f8f4dc0
Lee Roman <58044796+Velmor99@users.noreply.github.com>
2023-09-15 00:13:05 +0300

    done main pricing (#11)

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,5 @@
+import prices from '../../public/prices.json';
+
 export interface IFormatedData {
   title: string;
   content: string;
@@ -9,3 +11,14 @@ export interface IAdvancedHowItWorks {
 }
 
 export type IHowItWorks = IAdvancedHowItWorks | string;
+
+export interface ICoursesDataFormated {
+  level: IndividualLevelType | GroupLevelType;
+  duration: number;
+  pricePerHour: number;
+  allCoursePrice: number;
+  priceWithDiscount: number;
+}
+
+export type IndividualLevelType = keyof typeof prices.main.individual;
+export type GroupLevelType = keyof typeof prices.main.group;
