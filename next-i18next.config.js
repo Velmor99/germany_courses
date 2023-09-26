5d0e98940a7b2ee093190bfb062315fc1d058526
Velmor99 <wincentwangog@gmail.com>
2023-09-26 15:18:36 +0300

    change default locale on de

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   debug: process.env.NODE_ENV === 'development',
   i18n: {
     locales: ['de', 'ru'],
-    defaultLocale: 'ru',
+    defaultLocale: 'de',
     localeDetection: true,
   },
   trailingSlash: true,
